418ed2adfa1223d16e2b5f785cd07fb890fcecb7
unzagi <simon.unzagi@gmail.com>
2016-09-16 17:42:49 +0100

    - Cleaned up J02 Variable files - wrong 'outOfScope' information - added corresponding NAD files - created J01 Folder

--- JU01/jinja_ju01_variables.py
+++ JU01/jinja_ju01_variables.py
@@ -3,20 +3,20 @@ import time
 # Set the template specific description variables - information from NADS template excel spreadsheet
 hardwareMake = 'Juniper'
 hardwareModel = 'Juniper SRX300+SRX-MP-1SFP-GE'
-projectTemplateName = 'JU02'
-previousTemplateName = 'JN47'
+projectTemplateName = 'JU01'
+previousTemplateName = 'JN46'
 
 telcoName = 'TTB  Ethernet - 10Gb NNI'
 circuitName = 'EAD1000'
 bandwidth = '100-200Mbps'
 routing = 'BGP'
-connectionType = 'Core - B'
+connectionType = 'Core - A'
 customerLans = 1
 customerStaticRoutes = 0
 outOfScope = "DHCP, VPN, Backup/Redundancy, Secondary IP Addresses & ANYTHING else"
 
-outputFileName = "JU02-template.txt"
-jinja2TemplateName = 'JU02.jinja2'
+outputFileName = "JU01-template.txt"
+jinja2TemplateName = 'JU01.jinja2'
 dateNow = time.strftime("%c") # set the time/date
 engineerName = 'Simon Brooks'
 
